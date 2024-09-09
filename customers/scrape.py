8329a7f3ed4e226812bd52cc70291340887d6e17
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-09-09 22:47:47 +0800

    Create scrape.py

---
+++ customers/scrape.py
@@ -0,0 +1,19 @@
+import requests
+import csv
+
+customers = open("../db csv's/customers.csv", 'a')
+products_orders = open("db csv's/products-orders.csv", 'a')
+orders = open("db csv's/orders.csv", 'a')
+
+customers_writer = csv.writer(customers, quotechar='"')
+products_orders_writer = csv.writer(products_orders, quotechar='"')
+orders_writer = csv.writer(orders, quotechar='"')
+
+num = 1
+
+for i in range(0,num):
+    response = requests.get('https://random-person-generator.com/api?preset=personal-details&?country=us')
+    json = response.json()
+    customers_writer.writerow([i,json['identification']['full_name'], json['financial_information']['credit_card_number'], json['financial_information']['cvv2'], json['financial_information']['expiration_date']])
+    products_orders_writer.writerow()
+    orders_writer.writerow([json['contact_information']['home_address']])
