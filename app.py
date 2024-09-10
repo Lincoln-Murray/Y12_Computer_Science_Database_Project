ba7bc8c7c14b621ea7583d95751ff3ec8fb07ee9
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-09-10 22:24:34 +0800

    Flask implementation
    Started the flask implementation.

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import requests as r
+import sqlite3
+
+app = Flask(__name__)
+
+
+_products = []
+
+@app.route("/")
+def index():
+    conn = sqlite3.connect('sqlite (1).db')
+    curs = conn.cursor()
+    with open('sql/select_products.sql') as sql_select: curs.execute(sql_select.read())
+    for line in curs:
+        _products.append(line)
+    return render_template('index.html', products = _products)
