b8b266fc315bd445df3468ed00e1f5a1b81b949f
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-09-10 22:22:08 +0800

    Update scrape.py

--- customers/scrape.py
+++ customers/scrape.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+import random
 
 customers = open("../db csv's/customers.csv", 'a')
 products_orders = open("db csv's/products-orders.csv", 'a')
@@ -14,6 +15,7 @@ num = 1
 for i in range(0,num):
     response = requests.get('https://random-person-generator.com/api?preset=personal-details&?country=us')
     json = response.json()
-    customers_writer.writerow([i,json['identification']['full_name'], json['financial_information']['credit_card_number'], json['financial_information']['cvv2'], json['financial_information']['expiration_date']])
+    customers_writer.writerow([i,json['identification']['full_name'], json['contact_information']['home_address'], json['financial_information']['credit_card_number'], json['financial_information']['cvv2'], json['financial_information']['expiration_date']])
     products_orders_writer.writerow()
-    orders_writer.writerow([json['contact_information']['home_address']])
+for i in range(0, random.randint(num,2*num)):
+    orders_writer.writerow([i,json['contact_information']['home_address']])
