bfbd161f81e885744c3bf5d3064f7618d9eeca40
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-09-11 22:05:42 +0800

    Update scrape.py
    got customer list generated

--- customers/scrape.py
+++ customers/scrape.py
@@ -2,7 +2,7 @@ import requests
 import csv
 import random
 
-customers = open("../db csv's/customers.csv", 'a')
+customers = open("db csv's/customers.csv", 'a')
 products_orders = open("db csv's/products-orders.csv", 'a')
 orders = open("db csv's/orders.csv", 'a')
 
@@ -10,12 +10,30 @@ customers_writer = csv.writer(customers, quotechar='"')
 products_orders_writer = csv.writer(products_orders, quotechar='"')
 orders_writer = csv.writer(orders, quotechar='"')
 
-num = 1
+num = 85
 
-for i in range(0,num):
-    response = requests.get('https://random-person-generator.com/api?preset=personal-details&?country=us')
-    json = response.json()
-    customers_writer.writerow([i,json['identification']['full_name'], json['contact_information']['home_address'], json['financial_information']['credit_card_number'], json['financial_information']['cvv2'], json['financial_information']['expiration_date']])
-    products_orders_writer.writerow()
-for i in range(0, random.randint(num,2*num)):
-    orders_writer.writerow([i,json['contact_information']['home_address']])
+#customers_writer.writerow(['CustomerID','Name','Address','Card Number','CVV','Expiry'])
+#orders_writer.writerow(['OrderID','Date','Address','CustomerID'])
+#products_orders_writer.writerow(['Product ID', 'Order ID'])
+
+#modifier = 0
+#for i in range(0,num):
+#    try:
+#        response = requests.get('https://random-person-generator.com/api?preset=personal-details&?country=us')
+#        json = response.json()
+#        customers_writer.writerow([i+modifier,json['identification']['full_name'], json['contact_information']['home_address'], json['financial_information']['credit_card_number'], json['financial_information']['cvv2'], json['financial_information']['expiration_date']])
+#    except:
+#        modifier-=1
+
+#num+=modifier
+
+
+
+num2 = random.randint(num,2*num)
+for ii in range(0, num2):
+    orders_writer.writerow([ii, random.randint(0,num)])
+    products_orders_writer.writerow([random.randint(0,176),ii])
+
+for iii in range(0, num2):
+    if random.randint(1,5) <= 4:
+        products_orders_writer.writerow([random.randint(0,176),iii])
