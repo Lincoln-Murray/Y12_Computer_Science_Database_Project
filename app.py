4444aa0116c5dad9b210174569de199615622eb0
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-09-12 21:08:27 +0800

    Individual product pages

--- app.py
+++ app.py
@@ -7,11 +7,36 @@ app = Flask(__name__)
 
 _products = []
 
-@app.route("/")
-def index():
+@app.route("/<string:sort>", methods=['GET','POST'])
+def index(sort):
+    print(sort)
     conn = sqlite3.connect('sqlite (2).db')
     curs = conn.cursor()
-    with open('sql/select_products.sql') as sql_select: curs.execute(sql_select.read())
-    for line in curs:
-        _products.append(line)
+    if sort == 'price-min':
+        with open('sql/select_products.sql') as sql_select: curs.execute(sql_select.read()[:-1] + " order by 'productssupplier'.price ASc;")
+        for line in curs:
+            _products.append(line)
+    if sort == 'price-max':
+        with open('sql/select_products.sql') as sql_select: curs.execute(sql_select.read()[:-1] + " order by 'productssupplier'.price DESC;")
+        for line in curs:
+            _products.append(line)
+    else:
+        with open('sql/select_products.sql') as sql_select: curs.execute(sql_select.read())
+        for line in curs:
+            _products.append(line)
+    conn.commit()
+    conn.close()
+    print(render_template('index.html', products = _products))
     return render_template('index.html', products = _products)
+
+@app.route("/product<int:product_id>", methods=['GET','POST'])
+def product(product_id):
+    conn = sqlite3.connect('sqlite (2).db')
+    curs = conn.cursor()
+    with open('sql/select_products.sql') as sql_select: curs.execute(sql_select.read()[:-1] + " where 'products'.'product_id' = " + str(product_id) + ";")
+    for line in curs:
+        _product = line
+    print(_products)
+    conn.commit()
+    conn.close()
+    return render_template('product.html', product = _product)
