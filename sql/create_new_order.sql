0437d441c3a6725e6a7896a09049a69bf2238264
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-09-16 18:07:24 +0800

    SQL

---
+++ sql/create_new_order.sql
@@ -0,0 +1,2 @@
+INSERT into productsorders (Product_ID, Order_ID)
+values (-1,-2);
---
+++ sql/delete_product_from_order.sql
@@ -0,0 +1,4 @@
+DELETE
+from productsorders
+where productsorders.Product_ID = -1
+and productsorders.Order_ID = -2;
