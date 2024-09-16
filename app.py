ea7d1e59ec0c29c6dced14c1257dbc36b969f008
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-09-16 18:07:09 +0800

    Add/Remove items

--- app.py
+++ app.py
@@ -4,29 +4,32 @@ import sqlite3
 
 app = Flask(__name__)
 
-
+_order_id = 1
 
 @app.route("/<string:sort>", methods=['GET','POST'])
 def index(sort):
-    print(sort)
-    _products = []
-    conn = sqlite3.connect('Rock Scalers.db')
-    curs = conn.cursor()
-    if sort == 'price-min':
-        with open('sql/select_products.sql') as sql_select: curs.execute(sql_select.read()[:-1] + " order by 'productssupplier'.price ASc;")
-        for line in curs:
-            _products.append(line)
-    if sort == 'price-max':
-        with open('sql/select_products.sql') as sql_select: curs.execute(sql_select.read()[:-1] + " order by 'productssupplier'.price DESC;")
-        for line in curs:
-            _products.append(line)
+    if sort == 'cart':
+        global _order_id
+        return cart(_order_id)
     else:
-        with open('sql/select_products.sql') as sql_select: curs.execute(sql_select.read())
-        for line in curs:
-            _products.append(line)
-    conn.commit()
-    conn.close()
-    return render_template('index.html', products = _products)
+        _products = []
+        conn = sqlite3.connect('Rock Scalers.db')
+        curs = conn.cursor()
+        if sort == 'price-min':
+            with open('sql/select_products.sql') as sql_select: curs.execute(sql_select.read()[:-1] + " order by 'productssupplier'.price ASc;")
+            for line in curs:
+                _products.append(line)
+        if sort == 'price-max':
+            with open('sql/select_products.sql') as sql_select: curs.execute(sql_select.read()[:-1] + " order by 'productssupplier'.price DESC;")
+            for line in curs:
+                _products.append(line)
+        else:
+            with open('sql/select_products.sql') as sql_select: curs.execute(sql_select.read())
+            for line in curs:
+                _products.append(line)
+        conn.commit()
+        conn.close()
+        return render_template('index.html', products = _products)
 
 @app.route("/product<int:product_id>", methods=['GET','POST'])
 def product(product_id):
@@ -41,13 +44,33 @@ def product(product_id):
 
 @app.route("/cart<int:order_id>")
 def cart(order_id):
+    global _order_id
+    _order_id = order_id
     _products = []
     conn = sqlite3.connect('Rock Scalers.db')
     curs = conn.cursor()
-    with open('sql/select_cart_products.sql') as sql_select: curs.execute(sql_select.read()[:-1].replace(str(2), str(order_id)))
+    with open('sql/select_cart_products.sql') as sql_select: curs.execute(sql_select.read().replace(str(2), str(order_id)))
     for line in curs:
         _products.append(line)
-        print(line)
     conn.commit()
     conn.close()
-    return render_template('cart.html', products = _products)
+    return render_template('cart.html', products = _products, order_id = order_id)
+
+@app.route("/remove_p=<int:product_id>&o=<int:order_id>", methods=['GET','POST'])
+def remove_item(product_id, order_id):
+    conn = sqlite3.connect('Rock Scalers.db')
+    curs = conn.cursor()
+    with open('sql/delete_product_from_order.sql') as sql_select: curs.execute(sql_select.read().replace(str(-1), str(product_id)).replace(str(-2), str(order_id)))
+    conn.commit()
+    conn.close()
+    return cart(order_id)
+
+@app.route("/add_p=<int:product_id>")
+def add_item(product_id):
+    global _order_id
+    conn = sqlite3.connect('Rock Scalers.db')
+    curs = conn.cursor()
+    with open('sql/create_new_order.sql') as sql_select: curs.execute(sql_select.read().replace(str(-1), str(product_id)).replace(str(-2), str(_order_id)))
+    conn.commit()
+    conn.close()
+    return cart(_order_id)
