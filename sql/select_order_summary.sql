de947b0ac80dd1ae898f98d447971b2606251d79
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-09-17 15:33:15 +0800

    SQL files

--- sql/select_order_summary.sql
+++ sql/select_order_summary.sql
@@ -1,8 +1,9 @@
-select orders.OrderID, customers.Name, orders.Date, SUM(productssupplier.Price) as Total
+select orders.OrderID, customers.Name, orders.Date, SUM(productssupplier.Price)/2 as Total
 from orders
 join customers on orders.CustomerID = customers.CustomerID
 join productsorders on orders.OrderID = productsorders.Order_ID
 join products on productsorders.Product_ID = products.Product_ID
 join productssupplier on products.Product_ID = productssupplier.Product_ID
-GROUP by customers.CustomerID, orders.OrderID
+WHERE orders.OrderID = -2
+GROUP by orders.OrderID || customers.CustomerID
 order by customers.name;
--- sql/select_products.sql
+++ sql/select_products.sql
@@ -1,4 +1,5 @@
 SELECT 'products'.'product_id', 'products'.'product_name', 'products'.'Description', 'productssupplier'.price, 'manufacturer'.'name', 'productssupplier'.Image_URL
 FROM products
 join 'productssupplier' on 'products'.'Product_ID' = 'productssupplier'.'Product_ID'
-join 'manufacturer' on 'products'.'Manufacturer_ID' = 'manufacturer'.'Manufacturer_ID';
+join 'manufacturer' on 'products'.'Manufacturer_ID' = 'manufacturer'.'Manufacturer_ID'
+group by 'products'.'product_id';
